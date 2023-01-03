cf71999173c4905afd1485c99908ce7afc6c7159
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-02 20:00:16 -0600

    Adding magic_number.py file

---
+++ Examples/magic_number.py
@@ -0,0 +1,10 @@
+# magic_number.py, Chapter 5, Python Crash Course
+# program uses an if statement and inequality
+
+answer = 17
+
+if answer != 42:
+    print("That is not the correct answer. Please try again!")
+
+
+
