d6020380d421b5f81173e5df4592f5d72ae0bd90
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-02 19:51:10 -0600

    Adding toppings.py file

---
+++ Examples/toppings.py
@@ -0,0 +1,10 @@
+# toppings.py, Chapter 5, Python Crash Course
+# program uses a if statement and inequality
+
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
+
+
