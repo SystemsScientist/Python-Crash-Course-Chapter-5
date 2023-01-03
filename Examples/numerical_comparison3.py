b4b7eee1cf625d076b47ca864c8dbcb8c8ff21bf
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-02 20:10:14 -0600

    Adding numerical_comparison3.py file

---
+++ Examples/numerical_comparison3.py
@@ -0,0 +1,14 @@
+# numerical_comparison3.py, Chapter 5, Python Crash Course
+# program uses conditional tests to compare values
+
+age_0 = 22
+age_1 = 18
+
+print(age_0 >= 21 and age_1 >= 21) # prints False
+
+age_1 = 22
+
+print(age_0 >= 21 and age_1 >= 21) # prints True
+
+
+
