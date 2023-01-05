861b7912e7613d38e87fbb0bbcee113c5eba3366
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-04 19:10:39 -0600

    Adding voting2.py file

---
+++ Examples/voting2.py
@@ -0,0 +1,10 @@
+# voting2.py, Chapter 5, Python Crash Course
+# program uses an if statement to test a variable
+
+age = 19
+if age >= 18: 
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+
+
+
