db52b446d37d6b1342a9495a3a81cadc6b40a607
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-04 19:22:41 -0600

    Adding amusement_park.py file

---
+++ Examples/amusement_park.py
@@ -0,0 +1,14 @@
+# amusement_park.py, Chapter 5, Python Crash Course
+# program uses an if-elif-else block to test a variable
+
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5.")
+else:
+    print("Your admission cost is $10.")
+
+
+
