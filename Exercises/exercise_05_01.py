270e94fcb0386c6043bcd9f0e73a4a7db554ebde
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-04 18:25:52 -0600

    Adding exercise_05_01.py file

---
+++ Exercises/exercise_05_01.py
@@ -0,0 +1,77 @@
+# exercise_05_01.py, Chapter 05, Python Crash Course
+# 
+# Conditional Tests: Write a series of conditional tests.
+# Print a message describing each test and your prediction
+# for the results of each test. Your code should look 
+# something like this:
+# 
+# --------------------------------------------------------
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+#
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+# --------------------------------------------------------
+#
+
+truck = 'dodge'
+print("Is truck == 'dodge'? I predict True.")
+print(truck == 'dodge')
+
+print("\nIs truck == 'ford'? I predict False.")
+print(truck == 'ford')
+
+#   - Look closely at your results, and make sure you 
+#     understand why each line evaluates to True or False.
+#   - Create at least 10 tests. Have at least 5 tests
+#     evaluate to True and another 5 tests evaluate to
+#     False.
+
+print("\nTest 1 and Test 2")
+print("------------------------------------------------")
+color = 'blue'
+print("Is color == 'blue'? I predict True.")
+print(color == 'blue')
+
+print("\nIs color == 'red'? I predict False.")
+print(color == 'red')
+
+print("\nTest 3 and Test 4")
+print("-----------------------------------------------")
+programming_language = 'python'
+print("Is language == 'python'? I predict True.")
+print(programming_language == 'python')
+
+print("\nIs language == 'c++'? I predict False.")
+print(programming_language == 'c++')
+
+print("\nTest 5 and Test 6")
+print("----------------------------------------------")
+operating_system = 'linux'
+print("Is operating system != 'linux'? I predict False.")
+print(operating_system != 'linux')
+
+print("\nIs operating system != 'windows'? I predict True.")
+print(operating_system != 'windows')
+
+print("\nTest 7 and Test 8")
+print("---------------------------------------------")
+computer_scientist = 'dennis ritchie'
+print("Is computer scientist == 'dennis ritchie'? I predict True.")
+print(computer_scientist == 'dennis ritchie')
+
+print("\nIs computer scientist == 'joe biden'? I predict False.")
+print(computer_scientist == 'joe biden')
+
+print("\nTest 9 and Test 10")
+print("--------------------------------------------")
+programming = 'awesome'
+print("Is programming == 'awesome'? I predict True.")
+print(programming == 'awesome')
+
+print("\nIs programming == 'not awesome'? I predict False.")
+print(programming == 'not awesome')
+
+
+
