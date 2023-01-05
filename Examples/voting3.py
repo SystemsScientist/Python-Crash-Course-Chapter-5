070997f8c7f1f16fe2fd3fded2932b4bfe57c9be
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-04 19:16:03 -0600

    Adding voting3.py file

---
+++ Examples/voting3.py
@@ -0,0 +1,14 @@
+# voting3.py, Chapter 5, Python Crash Course
+# program uses an if-else statement to test a 
+# variable
+
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please registered to vote as soon as you turn 18!")
+
+
+
