56cd77837b067c328d4cb02cd06d1b2355f2cf94
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-07 17:51:07 -0600

    Adding toppings2.py file

---
+++ Examples/toppings2.py
@@ -0,0 +1,17 @@
+# toppings2.py, Chapter 5, Python Crash Course
+# program uses multiple if statements to determine
+# the requested topping
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+
+
