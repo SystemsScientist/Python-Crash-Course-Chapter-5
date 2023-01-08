d1fc3e7f62732034a1682e51fc10733592e1ef43
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-07 18:02:35 -0600

    Adding toppings3.py file

---
+++ Examples/toppings3.py
@@ -0,0 +1,17 @@
+# toppings3.py, Chapter 5, Python Crash Course
+# program uses an if-elif block and does not produce
+# the expected result
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+elif 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+
+
