8dea8a62ef59a065bc3000e0312ad41974ba0531
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-07 19:36:46 -0600

    Adding toppings4.py file

---
+++ Examples/toppings4.py
@@ -0,0 +1,13 @@
+# toppings4.py, Chapter 5, Python Crash Course
+# program uses a list and for loop to print
+# pizza toppings
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print("Adding " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
+
+
+
