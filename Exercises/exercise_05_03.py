3951e65ea90ce49497770be817160420fc8ffc64
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-07 18:12:09 -0600

    Adding exercise_05_03.py file

---
+++ Exercises/exercise_05_03.py
@@ -0,0 +1,23 @@
+# exercise_05_03.py, Chapter 5, Python Crash Course
+# Alien Colors #1: Imagine an alien was just shot down
+# in a game. Create a variable called alien_color and
+# assign it a value of 'green', 'yellow', or 'red'
+
+alien_color = 'green'
+
+#   - Write an if statement to test whether the alien's
+#     color is green. if it is, print a message that the
+#     player just earned 5 points.
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+#   - Write one version of this program that passes the
+#     if test and another that fails. (The version that
+#     fails will have no output.)
+
+if alien_color == 'red':
+    print()
+
+
+
